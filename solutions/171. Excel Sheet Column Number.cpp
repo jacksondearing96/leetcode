9093b838d38e98180b4a82178065c717c0cce436
Jackson Dearing <34879247+jacksondearing96@users.noreply.github.com>
2020-10-17 16:15:39 +1030

    8 ms

---
+++ solutions/171. Excel Sheet Column Number.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    int letterToNum(char c)
+    {
+        switch (c)
+        {
+            case 'A':
+                return 1;
+            case 'B':
+                return 2;
+            case 'C':
+                return 3;
+            case 'D':
+                return 4;
+            case 'E':
+                return 5;
+            case 'F':
+                return 6;
+            case 'G':
+                return 7;
+            case 'H':
+                return 8;
+            case 'I':
+                return 9;
+            case 'J':
+                return 10;
+            case 'K':
+                return 11;
+            case 'L':
+                return 12;
+            case 'M':
+                return 13;
+            case 'N':
+                return 14;
+            case 'O':
+                return 15;
+            case 'P':
+                return 16;
+            case 'Q':
+                return 17;
+            case 'R':
+                return 18;
+            case 'S':
+                return 19;
+            case 'T':
