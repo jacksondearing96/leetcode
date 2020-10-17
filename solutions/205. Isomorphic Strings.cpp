433944c6e19803c4bca246a2f8c8acb6cc6e37c5
Jackson Dearing <34879247+jacksondearing96@users.noreply.github.com>
2020-10-17 16:41:58 +1030

    16 ms

--- solutions/205. Isomorphic Strings.cpp
+++ solutions/205. Isomorphic Strings.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     
-    bool isIsomorphic(string s, string t) {
-        
-        for (int i=0 ; i<s.length() ; i++)
-        {
-            for (int j=i+1 ; j<s.length() ; j++)
-            {
-                if (s[i] == s[j] && t[i] != t[j])
-                    return false;
-                
-                if (t[i] == t[j] && s[i] != s[j])
-                    return false;
-            }
+    string getCode(string s) {
+        unordered_map<char, int> seen;
+        string code;   
+        for (int i = 0; i < s.length(); ++i) {
+            if (seen.find(s[i]) == seen.end()) seen[s[i]] = seen.size();
+            code.push_back(char(seen[s[i]]));
         }
-        return true;
+        return code;
+    }
+    
+    bool isIsomorphic(string s, string t) {
+        return getCode(s) == getCode(t);
     }
 };
