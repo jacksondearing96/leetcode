0f4483c54f628960b8a7d45189047d55bc58c4d3
Jackson Dearing <34879247+jacksondearing96@users.noreply.github.com>
2020-10-17 15:52:35 +1030

    1244 ms

---
+++ solutions/1. Two Sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        std::vector<int> returnVec;
+        
+        for (int i=0 ; i<nums.size()-1 ; i++)
+        {
+            for (int j=i+1 ; j<nums.size() ; j++)
+            {
+                if (nums.at(i) + nums.at(j) == target)
+                {
+                    returnVec.push_back(i);
+                    returnVec.push_back(j);
+                    return returnVec;
+                }
+            }
+        }
+        return {}; 
+    }
+};
