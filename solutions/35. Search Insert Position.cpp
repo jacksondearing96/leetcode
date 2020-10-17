155e4d373b899cd9c6ea64f98761a4253d12bd7e
Jackson Dearing <34879247+jacksondearing96@users.noreply.github.com>
2020-10-17 16:07:08 +1030

    4 ms

---
+++ solutions/35. Search Insert Position.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int size = nums.size();
+        for (int i=0 ; i<size ; i++)
+        {
+            if (nums.at(i) >= target)
+                return i;
+        }
+        
+        return size;
+    }
+};
