17de20eb61956337d8929ad8319f030b5c2a40a8
Jackson Dearing <34879247+jacksondearing96@users.noreply.github.com>
2021-07-22 20:25:56 +1000

    4 ms

--- solutions/92. Reverse Linked List II.cpp
+++ solutions/92. Reverse Linked List II.cpp
@@ -10,7 +10,49 @@
  */
 class Solution {
 public:
+    void print(ListNode* head) {
+        if (head == nullptr) {
+            cout << endl;
+            return;
+        }
+        cout << head->val << ",";
+        print(head->next);
+    }
+    
+    ListNode* reverse(ListNode* head, ListNode* prev = nullptr) {
+        if (head == nullptr) return prev;
+        ListNode* next = head->next;
+        head->next = prev;
+        return reverse(next, head);
+    }
+    
     ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (head == nullptr) return nullptr;
         
+        ListNode* tail = head;
+        ListNode* prevToTail = nullptr;
+        while (right != 0) {
+            prevToTail = tail;
+            tail = tail->next;
+            --right;
+        }
+        
+        ListNode* leftStart = head;
+        ListNode* prevToLeftStart = nullptr;
+        while(left != 1) {
+            prevToLeftStart = leftStart;
+            leftStart = leftStart->next;
+            --left;
+        }
+        
+        prevToTail->next = nullptr;
+        if (prevToLeftStart == nullptr) {
+            head = reverse(leftStart);
+        } else {
+            prevToLeftStart->next = reverse(leftStart);
+        }
+        leftStart->next = tail;
+        
+        return head;
     }
 };
